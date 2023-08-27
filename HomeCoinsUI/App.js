c59ef6f5fa842951cce23570104ecf4a6a1d5634
Banty <mr.bantikumar9716@gmail.com>
2023-08-27 23:49:06 +0530

    create login screens

--- HomeCoinsUI/App.js
+++ HomeCoinsUI/App.js
@@ -3,63 +3,20 @@
  * @BANTI
  */
 
+import { NavigationContainer } from '@react-navigation/native';
+
 import React from 'react';
-// import type { PropsWithChildren } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
-import { Header } from 'react-native-elements';
-import { Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-function Section({ children, title }){
-  const isDarkMode = useColorScheme() === 'dark';
+import Login from './Screens/Users/Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+function App(){  
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.sectionContainer}>
-      <Text style={[styles.sectionTitle,{ color: isDarkMode ? Colors.white : Colors.black}]}>
-        {title}
-      </Text>
-      <Text style={[styles.sectionDescription,{color: isDarkMode ? Colors.light : Colors.dark,},]}>
-        {children}
-      </Text>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login'>
+          <Stack.Screen name='Login' component={Login} options={{headerShown:false}}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-function App(){
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundStyle.backgroundColor}/>
-      <ScrollView showsHorizontalScrollIndicator={false} contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-        <View></View>
-      </ScrollView>     
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
