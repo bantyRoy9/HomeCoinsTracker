f76d1e7c49b875711add07cf5ae035ec50c63517
Banty <mr.bantikumar9716@gmail.com>
2023-09-02 08:44:04 +0530

    create login screen

---
+++ HomeCoinsUI/Components/Input.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+
+const Input = ({props}) => {
+  return (
+    <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>{props.label}</Text>
+        <TextInput 
+                value= {props.email}
+                style={props?.styles??styles.inputBox}
+                autoFocus={props.autoFocus}
+                label={props.label}
+                cursorColor={"#F2F2F3"}
+                //selectionColor={"blue"}
+                secureTextEntry={props?.secureTextEntry??false}
+                placeholder={props.placeholder}
+              />
+    </View>
+  )
+}
+
+export default Input
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        marginHorizontal:15,
+        marginVertical:10,
+    },
+    inputLabel:{
+        color: 'white',
+        fontWeight: 'bold', 
+        fontSize: 18,
+        marginBottom: 5
+    },
+    inputBox:{
+        backgroundColor:"#38304C",
+        borderRadius:15,
+        padding:15,
+        fontSize:18
+    }
+})
--- HomeCoinsUI/Screens/Users/Login.jsx
+++ HomeCoinsUI/Screens/Users/Login.jsx
@@ -1,6 +1,10 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, useColorScheme } from 'react-native'
-import React from 'react'
+import { Button, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View, useColorScheme } from 'react-native'
+import React, { useState } from 'react'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { StatusBar } from 'react-native';
+import { TextInput } from 'react-native';
+import { Image } from 'react-native';
+import Input from '../../Components/Input';
 
 
 function Section({ children, title }){
@@ -19,15 +23,39 @@ function Section({ children, title }){
   
 const Login = () => {
     const isDarkMode = useColorScheme() === 'dark';
+    Colors.darker = "#201A31";
     const backgroundStyle = {
       backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     };
+
+    const [user,setUser] = useState({email:"",password:""});
+
     return (
     <SafeAreaView style={backgroundStyle}>
-      {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundStyle.backgroundColor}/> */}
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundStyle.backgroundColor}/>
       <ScrollView showsHorizontalScrollIndicator={false} contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
         <View>
-            <Text style={{fontSize:30}}>HJK</Text>
+            <View>
+              <Image source={require('../../Assets/Icons/login.webp')}
+              style={{width:'auto',height:300}}
+              />
+            </View>
+            <View>
+              <Input props={{placeholder:"Email", label:"Email",value:user.email,autoFocus:false}}/>
+            </View>
+            <View>
+              <Input props={{secureTextEntry:true,placeholder:"Enter secure password", label:"Password",value:user.password,autoFocus:false}}/>
+            </View>
+            <View style={{width:"auto"}}>
+              <Pressable style={styles.button} onPress={""}>
+                <Text style={styles.text}>{"LOGIN"}</Text>
+              </Pressable>
+            </View>
+            <View style={{height:100}}>
+              <View>
+
+              </View>
+            </View>
         </View>
       </ScrollView>     
     </SafeAreaView>
@@ -38,6 +66,28 @@ export default Login
 
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 18,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+    elevation: 3,
+    backgroundColor: '#0DF5E3',
+    width:"40%",
+    marginLeft:'28%',
+    marginVertical:15
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+    inputBox:{
+      backgroundColor:"#38304D"
+    },
     sectionContainer: {
       marginTop: 32,
       paddingHorizontal: 24,
