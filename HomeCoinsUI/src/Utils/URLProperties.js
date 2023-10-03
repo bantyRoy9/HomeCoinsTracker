b73a10d66383713711a1a322aff5bc8322078512
Banty <mr.bantikumar9716@gmail.com>
2023-10-03 23:23:56 +0530

    create activity models

--- HomeCoinsUI/src/Utils/URLProperties.js
+++ HomeCoinsUI/src/Utils/URLProperties.js
@@ -1,7 +1,7 @@
 const NODE_ENV = "development"
-// const REACT_LOCAL_URL = "http://192.168.1.12:8000"
+const REACT_LOCAL_URL = "http://192.168.1.12:8000"
 // const REACT_LOCAL_URL = "http://192.168.43.167:8000"
-const REACT_LOCAL_URL = "http://192.168.1.14:8000"
+//const REACT_LOCAL_URL = "http://192.168.1.14:8000"
 REACT_PROD_URL = "https://homecoinstracker.onrender.com"
 export const accountControllerURL = `${NODE_ENV == 'production' ? REACT_PROD_URL:REACT_LOCAL_URL}/api/v1/accountController`;
 export const userControllerURL = `${NODE_ENV == 'production' ? REACT_PROD_URL:REACT_LOCAL_URL}/api/v1/userController`
--- Routes/accountRoute.js
+++ Routes/accountRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { saveDailyEarns,getTotalEarns, totalEarnByUser,getTotalExpend,saveDailyExped,getQuery } = require('../Controllers/accountController');
+const { saveDailyEarns, getTotalEarns, totalEarnByUser, getTotalExpend, saveDailyExped, getQuery } = require('../Controllers/accountController');
 const { protect, getUserId } = require('../Controllers/authController');
 const router = express.Router();
 
-router.route('/getEarnExpend').get((req,res,next)=>{console.log('/getEarnExpend');next();},getQuery,getTotalEarns,getTotalExpend)
-router.route('/earn').get((req,res,next)=>{console.log('/earn');next();},getTotalEarns).post(protect,getUserId('earnBy'), saveDailyEarns);
-router.route('/getEarnByUser').get((req,res,next)=>{console.log('/getEarnExpend');next();},protect,totalEarnByUser);
-router.route('/expend').get((req,res,next)=>{console.log('/expend');next();},getTotalExpend).post((req,res,next)=>{console.log('/getEarnExpend');next();},protect,getUserId('expendBy'), saveDailyExped);
+router.route('/getEarnExpend').get((req, res, next) => { console.log('/getEarnExpend'); next(); }, getQuery, getTotalEarns, getTotalExpend)
+router.route('/earn').get((req, res, next) => { console.log('/earn'); next(); }, getTotalEarns).post(protect, getUserId('earnBy'), saveDailyEarns);
+router.route('/getEarnByUser').get((req, res, next) => { console.log('/getEarnExpend'); next(); }, protect, totalEarnByUser);
+router.route('/expend').get((req, res, next) => { console.log('/expend'); next(); }, getTotalExpend).post((req, res, next) => { console.log('/getEarnExpend'); next(); }, protect, getUserId('expendBy'), saveDailyExped);
 
 
 module.exports = router
