35665cc980350237040876732099d3cc6648733e
Banty <mr.bantikumar9716@gmail.com>
2023-10-21 23:44:17 +0530

    handle errors

---
+++ HomeCoinsUI/src/Components/index.jsx
@@ -0,0 +1,8 @@
+import Chart from './Chart';
+import DataTable from './DataTable';
+import DatePicker from './DatePicker';
+import FloatingActionBtn from './FloatingActionBtn';
+import Header from './Header';
+import Input from './Input';
+import Modals from './Modal';
+export { Chart, DataTable, DatePicker, FloatingActionBtn, Header, Input, Modals}
--- HomeCoinsUI/src/Utils/VectorIcons.js
+++ HomeCoinsUI/src/Utils/VectorIcons.js
@@ -2,4 +2,5 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-export { FontAwesome, FontAwesome5,AntDesign,Ionicons}
+import FeatherIcons from 'react-native-vector-icons/Feather';
+export { FontAwesome, FontAwesome5,AntDesign,Ionicons,FeatherIcons}
