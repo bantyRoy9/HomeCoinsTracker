3dedbc0bc2e78d802d89b3eb8ede7dd2f2e64889
Banty <mr.bantikumar9716@gmail.com>
2023-10-29 00:41:36 +0530

    handle email varification

--- Routes/groupRoute.js
+++ Routes/groupRoute.js
@@ -3,10 +3,9 @@ const { createGroup,addMembers,getGroupList, addMemberRequest,verifyGroupToken }
 
 const router = require('express').Router();
 
+router.route('/verifyUser/:verifyToken').get(verifyGroupToken,addMembers);
 router.use(protect);
 router.route('/group').get(restrictTo('admin'),getGroupList).post(createGroup);
 
 router.route('/group/:id').post(protect, addMemberRequest);
-router.route('/verifyUser/:verifyToken').get(verifyGroupToken,addMembers);
-// http://192.168.137.1:8000/api/v1/groupController/verifyUser/2427fe16735a28a546e0d651d747bf4de8162586a7a881f2592f22f8da5c1f05
 module.exports = router
