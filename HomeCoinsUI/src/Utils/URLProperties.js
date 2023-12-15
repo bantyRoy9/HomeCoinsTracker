69341a6d81b5bd1a4ed27d31dd48ab68f8c68990
Banty <mr.bantikumar9716@gmail.com>
2023-12-15 23:44:28 +0530

    add pointerevents

--- HomeCoinsUI/src/Utils/URLProperties.js
+++ HomeCoinsUI/src/Utils/URLProperties.js
@@ -1,5 +1,4 @@
 const NODE_ENV = process.env.NODE_ENV
-console.log(NODE_ENV)
 // const REACT_LOCAL_URL = "http://192.168.1.5:8000" //manikarn
 // const REACT_LOCAL_URL = "http://192.168.43.167:8000" //manikarn
 const REACT_LOCAL_URL = "http://192.168.1.7:8000" //note7
