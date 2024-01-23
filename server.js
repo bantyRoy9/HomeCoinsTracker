9d26f4f68634662aff3c63083f40b596d492d41e
Banty <87535171+bantyRoy9@users.noreply.github.com>
2024-01-23 21:41:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const app = require('./app');
-const dotenv = require('dotenv').config({path: './config.env'});
+const dotenv = require('dotenv').config({path: './.env'});
 const mongoose = require('mongoose');
 let DB = process.env.DB_URL;
 if(process.env.NODE_ENV === 'development'){
@@ -8,11 +8,11 @@ if(process.env.NODE_ENV === 'development'){
 };
 const PORT = process.env.PORT || 8000;
 mongoose.connect(DB).then( connection =>{
-    console.log('DB Connected',process.env.NODE_ENV);
+    console.log('DB Connected');
 }).catch( err=>{
     console.log(err);
 });
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Server is running on ${PORT}`,process.env.NODE_ENV);
 });
