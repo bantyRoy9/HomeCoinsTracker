39338ba76c50c129cc43cb18353941ce0172ac17
Banty <87535171+bantyRoy9@users.noreply.github.com>
2024-05-09 08:20:57 +0000

    handle source and expend as per user

--- Controllers/sourceController.js
+++ Controllers/sourceController.js
@@ -2,7 +2,7 @@ const expendType = require("../Model/AccountModels/expendTypeSchema");
 const sourceModel = require("../Model/SourceModels/sourceSchema");
 const { findModal, createModal } = require("./commonController");
 
-exports.getSource = findModal(sourceModel,{},"sourceName");
+exports.getSource = findModal(sourceModel,{groupId:true},"sourceName");
 exports.createSource = createModal(sourceModel);
-exports.getExpendType=findModal(expendType,{},"expendName")
+exports.getExpendType=findModal(expendType,{groupId:true},"expendName")
 exports.createExpendType = createModal(expendType);
